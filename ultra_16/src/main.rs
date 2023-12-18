96cc411b861ee00fc3fce67f496c364b616b84d8
Nicholas Londey <londey@gmail.com>
2023-12-18 20:50:32 +0000

    Split lib and main

--- ultra_16/src/main.rs
+++ ultra_16/src/main.rs
@@ -1,39 +1,6 @@
-use rust_hdl::prelude::*;
-use rust_hdl_bsp_alchitry_cu::pins::CLOCK_SPEED_100MHZ;
-use rust_hdl_bsp_alchitry_cu::{pins, synth};
-use std::time::Duration;
-
-#[derive(LogicBlock)]
-pub struct Blinky {
-  pulser: Pulser,
-  clock: Signal<In, Clock>,
-  leds: Signal<Out, Bits<8>>,
-}
-
-impl Logic for Blinky {
-  #[hdl_gen]
-  fn update(&mut self) {
-    self.pulser.enable.next = true;
-    self.pulser.clock.next = self.clock.val();
-    self.leds.next = 0x00.into();
-    if self.pulser.pulse.val() {
-      self.leds.next = 0xAA.into();
-    }
-  }
-}
-
-impl Default for Blinky {
-  fn default() -> Self {
-    let pulser = Pulser::new(CLOCK_SPEED_100MHZ.into(), 1.0, Duration::from_millis(250));
-    Blinky {
-      pulser,
-      clock: pins::clock(),
-      leds: pins::leds(),
-    }
-  }
-}
+#![cfg_attr(not(debug_assertions), deny(warnings))]
 
 fn main() {
-    let uut = Blinky::default();
+    let uut = ultra_16_gs::Blinky::default();
     rust_hdl_bsp_alchitry_cu::synth::generate_bitstream(uut, "firmware/blinky")
 }
